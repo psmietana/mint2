1c5ba38e77ae62e3f0d238d4050ef38c720f72a8
psmietana <iammesser@gmail.com>
2020-09-21 21:25:02 +0200

    fixed fixtures to save encoded password

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -5,17 +5,27 @@ namespace App\DataFixtures;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class UserFixtures extends Fixture
 {
+    private $userPasswordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $userPasswordEncoder)
+    {
+        $this->userPasswordEncoder = $userPasswordEncoder;
+    }
+
     public function load(ObjectManager $manager)
     {
         $totalUsersNumber = rand(30, 50);
         for ($i = 1; $i <= $totalUsersNumber; $i++) {
-            $name = 'test' . $i;
             $user = new User();
+
+            $name = 'test' . $i;
+            $password = $this->userPasswordEncoder->encodePassword($user, $name);
             $user->setEmail($name . '@test.com');
-            $user->setPassword($name);
+            $user->setPassword($password);
             $user->setRoles(['ROLE_ADMIN']);
 
             $manager->persist($user);
